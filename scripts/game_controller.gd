ccbd3e2f05f37f755fcf3cd8bbc0545b0d50ea38
flowerFawn <georgejparker08@Gmail.com>
2026-01-10 17:25:53 +0000

    and the flower spawn rate
    I need to start testing again before I commit. whoops again also

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -37,7 +37,7 @@ func update_directions() -> void:
 
 func do_spawns() -> void:
 	spawn_enemies()
-	if GameInfo.rnd.randf() <= 1.01:
+	if GameInfo.rnd.randf() <= 0.01:
 		spawn_flower()
 
 func spawn_enemies() -> void:
