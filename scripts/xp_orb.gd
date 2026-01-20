b62113825737ffb005e61828b17f50ed7120d51c
flowerFawn <georgejparker08@Gmail.com>
2026-01-20 16:03:13 +0000

    xp orbs now follow the players position when being picked up

--- scripts/xp_orb.gd
+++ scripts/xp_orb.gd
@@ -26,9 +26,13 @@ func be_picked_up(body:Node2D) -> void:
 	var pickup_tween:Tween = create_tween()
 	#could be squared, but gets weird at long idstances
 	var distance_to_player:float = global_position.distance_to(body.global_position)
-	collision_node.disabled = true
+	var original_global_position = global_position
+	collision_node.queue_free()
 	pickup_tween.set_trans(Tween.TRANS_BACK)
-	pickup_tween.tween_property(self, "global_position", GameInfo.player_position, distance_to_player * 0.00142)
+	get_parent().remove_child(self)
+	GameInfo.player.add_child(self)
+	global_position = original_global_position
+	pickup_tween.tween_property(self, "position", Vector2.ZERO, distance_to_player * 0.00142)
 	await pickup_tween.finished
 	AudioHandler.play_sound(pick_up_noise, Vector2.ZERO, 2, 1)
 	SpellShop.spell_xp += xp_value
