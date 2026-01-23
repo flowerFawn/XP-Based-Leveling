fc93c24fde388e909cb1fba40f0617196aa07a84
flowerFawn <georgejparker08@Gmail.com>
2026-01-23 09:40:23 +0000

    decreased default input buffer setting

--- scripts/resources/base resources/config_options.gd
+++ scripts/resources/base resources/config_options.gd
@@ -2,6 +2,6 @@ extends Resource
 class_name ConfigOptions
 
 @export var skip_intro:bool = false
-@export var input_buffer:float = 0.5
+@export var input_buffer:float = 0.1
 @export var screen_mode:int = 3
 @export var show_quadtree:bool = false
